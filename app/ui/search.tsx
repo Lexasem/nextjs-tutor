3917f3dc5b50fabbb9da6788ca02d7bfd6465077
Alexey Semerenko <asemerenko@buka.ru>
2023-11-25 16:00:10 +0300

    add debouncing logic to Search component

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,21 +1,22 @@
 'use client';
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
-import {usePathname, useSearchParams, useRouter} from "next/navigation";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({placeholder}: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const {replace} = useRouter();
-    function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term: string) => {
         const params = new URLSearchParams(searchParams);
-        if(term){
+        if (term) {
             params.set('query', term);
-        }else{
+        } else {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
 
     return (
         <div className="relative flex flex-1 flex-shrink-0">
