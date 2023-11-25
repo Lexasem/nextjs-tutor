60b23332146fbdd7af6f0a84127e36c7888fb793
Alexey Semerenko <asemerenko@buka.ru>
2023-11-25 15:56:16 +0300

    enable table component

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,12 +1,23 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+import {CreateInvoice} from '@/app/ui/invoices/buttons';
+import {lusitana} from '@/app/ui/fonts';
+import {InvoicesTableSkeleton} from '@/app/ui/skeletons';
+import {Suspense} from 'react';
 
-export default async function Page() {
+export default async function Page(
+    {
+        searchParams,
+    }: {
+        searchParams?: {
+            query?: string;
+            page?: string;
+        };
+    }
+) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
 
     return (
         <div className="w-full">
@@ -14,12 +25,12 @@ export default async function Page() {
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
-                <CreateInvoice />
+                <Search placeholder="Search invoices..."/>
+                <CreateInvoice/>
             </div>
-            {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+                <Table query={query} currentPage={currentPage}/>
+            </Suspense>
             <div className="mt-5 flex w-full justify-center">
                 {/* <Pagination totalPages={totalPages} /> */}
             </div>
